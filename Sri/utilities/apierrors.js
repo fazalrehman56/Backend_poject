5c0a5490f0914d6b9fee61ebec8aaf7655976425
fazal rehman <fr7479960@gmail.com>
2026-08-29 01:28:02 +0500

    apierroes

---
+++ Sri/utilities/apierrors.js
@@ -0,0 +1,22 @@
+class ApiError extends Error{
+
+    constructor(
+        statusCode,
+        msg = "something went worng",
+        error=[],
+        stack=""
+    ){
+        super(msg)
+        this.statusCode = statusCode
+        this.data = null
+        this.error = error
+        this.success = false
+
+        if(stack){
+            this.stack = stack
+        }
+        else{
+             error.capturestacktrace(this,this.constructor)
+        }
+    }
+}
