87884c468d0656a7b9fe3122e1b8caf6311dd2b2
fazal rehman <fr7479960@gmail.com>
2026-09-05 02:22:10 +0500

    controller

--- Sri/controller/users.controller.js
+++ Sri/controller/users.controller.js
@@ -1,5 +1,8 @@
 import asyncHandler from "../utilities/asynchandler.js";
-import { ApiError } from "../utilities/apierrors.js";
+import  ApiError from "../utilities/apierrors.js";
+import { User } from "../models/user.models.js";
+import { uploadCloudinary } from "../utilities/cloudinary.js";
+import { ApiResponse } from "../utilities/ResponseApi.js";
 const registerUsers = asyncHandler(async(req,res)=>{
      // take user details from frontend
      // validation - no empty
@@ -9,18 +12,50 @@ const registerUsers = asyncHandler(async(req,res)=>{
      //create user object & create entry in data base
      // remove password and refresh token 
      // return repsonse
-     const {fullName,email,userName,Password} = req.body()
-     console.log("emial is ",email)
-     if([fullName,email,userName,Password].some((fileds)=> 
-      fileds?.trim()===""
-    )){
-        throw new ApiError(300,"all fields are required")
-    }
-
+     const {fullName,email,userName,Password} = req.body
+     console.log("email is ",email)
      
+     if([fullName,email,userName,Password].some((fileds)=>
+      fileds?.trim()===""
+     )){
+          throw new ApiError(501,"All Fields are requird")
+     }
 
+     const existedUSer = await User.findOne({
+          $or :[{userName},{email}]
+     })
+     if(existedUSer){
+          throw new ApiError(400,"THE User is Already Exist BROOO")
+     }
+     const localAvatorPath= req.files?.avator[0]
+     const localCoverImgPath= req.files?.avator[0]
 
+     if(localAvatorPath){
+          throw new ApiError(500,"avator field requird")
+     }
+     const avator = await uploadCloudinary(localAvatorPath)
+     const CoverImg = await uploadCloudinary(localCoverImgPath)
 
+     if(!avator){
+            throw new ApiError(500,"avator field requird")
+     }
+    const user = await User.create({
+          userName,
+          fullName,
+          Password,
+          email,
+          avator : avator.url,
+          coverImg : CoverImg.url
 
+     })
+     const createdUser = user.findbyId(user._id).select(
+          "-password -refresh_Token"
+     )
+     if(!createdUser){
+          throw new ApiError(500,"something went worng while register the user")
+     }
+   res.status(201).json(
+     new ApiResponse(200,createdUser,"user register successfully")
+   )
      })
 export  default registerUsers;
