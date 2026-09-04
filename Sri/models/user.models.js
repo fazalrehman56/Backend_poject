18ef525c1d36df42a61cf0e3898188520a349907
fazal rehman <fr7479960@gmail.com>
2026-09-05 02:22:20 +0500

    tokens

--- Sri/models/user.models.js
+++ Sri/models/user.models.js
@@ -33,6 +33,9 @@ const userSchema = new mongoose.Schema(
         type : String, 
         required : true,
         required :[true,"password field is required !"]
+     },
+    refresh_Token: {
+          type : String
      }
    },{timestamps:true})
 
