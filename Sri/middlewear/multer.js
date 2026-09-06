37b3f4f809feb24e8ffe906222ef86d5400050bb
fazal rehman <fr7479960@gmail.com>
2026-09-06 19:29:02 +0500

    middlewear

--- Sri/middlewear/multer.js
+++ Sri/middlewear/multer.js
@@ -2,14 +2,14 @@ import multer from "multer";
 
 const storage = multer.diskStorage({ //disk m store hohi file
   destination: function (req, file, cb) { // cb mtlb ye batata hn multer ko k kia resuly aya no need of return
-    if (someErrorCondition) {
-        return cb(new Error("Something went wrong"))
-    }
+    // if (someErrorCondition) {
+    //     return cb(new Error("Something went wrong"))
+    // }
     cb(null, './public/temp') // // file kha save hongi
 },
   filename: function (req, file, cb) { //file kis name say save hogi
    
-      if (err) return cb(err)
+      // if (err) return cb(err)
       cb(null, file.originalname)
     
   }
